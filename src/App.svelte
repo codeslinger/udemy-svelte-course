49a6be166acb02da217eb30fb631e4442017072b
Toby DiPasquale <toby@cbcg.net>
2021-05-30 22:15:35 -0400

    completed video #12

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,20 @@
+<!-- Logic -->
 <script>
-	export let name;
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<i class="fas fa-home fa-5x"></i>
-</main>
-
+<!-- Styling -->
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+
+<!-- Markup -->
+<nav class="nav">
+	<div class="nav-center">
+		<h1 class="nav-title">Budget Calculator</h1>
+		<button type="button" class="nav-btn">
+			<i class="far fa-plus-square"></i>
+			Add Item
+		</button>
+	</div>
+</nav>
