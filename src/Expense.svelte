cb534f400ed8f8f68d701104bed5acf83dd67fe0
Toby DiPasquale <toby@cbcg.net>
2021-06-03 19:25:43 -0400

    completed video #21

--- src/Expense.svelte
+++ src/Expense.svelte
@@ -4,7 +4,22 @@
     export let amount = 0;
 </script>
 
-<div>
-    <h1>title: {name}</h1>
-    <h2>amount: {amount}</h2>
-</div>
+<article class="single-expense">
+    <div class="expense-info">
+        <h2>
+            {name}
+            <button class="amount-btn">
+                <i class="fas fa-caret-down" />
+            </button>
+        </h2>
+        <h4>Amount: ${amount}</h4>
+    </div>
+    <div class="expense-buttons">
+        <button class="expense-btn edit-btn">
+            <i class="fas fa-pen" />
+        </button>
+        <button class="expense-btn delete-btn">
+            <i class="fas fa-trash" />
+        </button>
+    </div>
+</article>
