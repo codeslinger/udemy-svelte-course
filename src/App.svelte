18d7225f1c066c199b279a5493c1bf4324d7c532
Toby DiPasquale <toby@cbcg.net>
2021-06-03 20:55:55 -0400

    completed video #29

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,16 @@
 	// components
 	import Navbar from './Navbar.svelte';
 	import ExpenseList from './ExpenseList.svelte';
+	import Totals from './Totals.svelte';
 
 	// data
 	import expensesData from './expenses';
 
 	// variables
 	let expenses = [...expensesData];
+
+	// reactive
+	$: total = expenses.reduce((accum, elem) => accum + elem.amount, 0);
 
 	// functions
 	function removeExpense(id) {
@@ -25,6 +29,7 @@
 
 <Navbar />
 <main class="content">
+	<Totals title="total expenses" {total} />
 	<ExpenseList {expenses} />
 	<button type="button" class="btn btn-primary btn-block" on:click={clearExpenses}>
 		clear expenses
